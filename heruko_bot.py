92795e85933fb6a1acc0f58569d36a5a1b927d14
savaskarakus <savaskarakus@guest-wireless-upc-1605-10-120-082-017.usc.edu>
2018-03-24 11:36:18 -0700

    changing message

--- heruko_bot.py
+++ heruko_bot.py
@@ -18,13 +18,9 @@ access_token_secret= os.environ['access_token_secret']
 
 # Quotes to Tweet
 happy_quotes = [
-    "For every minute you are angry you lose sixty seconds of happiness. - Ralph Waldo Emerson",
-    "Folks are usually about as happy as they make their minds up to be. - Abraham Lincoln",
-    "Happiness is when what you think, what you say, and what you do are in harmony. - Mahatma Gandhi",
-    "Count your age by friends, not years. Count your life by smiles, not tears. - John Lennon",
-    "Happiness is a warm puppy. - Charles M. Schulz",
-    "The happiness of your life depends upon the quality of your thoughts. - Marcus Aurelius",
-    "Now and then it's good to pause in our pursuit of happiness and just be happy. - Guillaume Apollinaire"]
+    "Working on Heruko bot",
+    "Tweets are sent by heroku automatically "
+    ]
 
 
 # Create function for tweeting
