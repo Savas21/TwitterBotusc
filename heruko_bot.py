6119d49aeddb567ace1aac4db434f48370ff6b0d
savaskarakus <savaskarakus@guest-wireless-upc-1605-10-120-082-017.usc.edu>
2018-03-24 11:33:23 -0700

    changes3

--- heruko_bot.py
+++ heruko_bot.py
@@ -8,10 +8,10 @@ import os
 
 
 # Twitter API Keys. Place your keys here.
-consumer_key= os.environ['']
-consumer_secret= os.environ['']
-access_token= os.environ['']
-access_token_secret= os.environ['']
+consumer_key= os.environ['consumer_key']
+consumer_secret= os.environ['consumer_secret']
+access_token= os.environ['access_token']
+access_token_secret= os.environ['access_token_secret']
 
 
 
